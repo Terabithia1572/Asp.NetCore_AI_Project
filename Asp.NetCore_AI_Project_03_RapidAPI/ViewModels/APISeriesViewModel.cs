035ef5d48115531d5a58b40ed6aab9e599c06116
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 16:17:02 +0300

    APISeriesViewModel Process Added

---
+++ Asp.NetCore_AI_Project_03_RapidAPI/ViewModels/APISeriesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore_AI_Project_03_RapidAPI.ViewModels
+{
+    public class APISeriesViewModel
+    {
+        public int rank { get; set; }
+        public string title { get; set; }
+        public float rating { get; set; }
+        public string year { get; set; }
+     
+    }
+
+
+}
