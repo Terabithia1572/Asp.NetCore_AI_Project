dca140303c07b40ade5e1c3a72c8f8862202a359
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 14:12:40 +0300

    artificial intelligence Project Created +Installing Entity Framework Packages Added

---
+++ Asp.NetCore_AI_Project_01_APIDemo/Entites/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace Asp.NetCore_AI_Project_01_APIDemo.Entites
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; } // Müşteri ID
+        public string CustomerName { get; set; } // Müşteri Adı
+        public string CustomerSurname { get; set; } // Müşteri Soyadı
+        public decimal CustomerBalance { get; set; } // Müşteri Bakiyesi
+    }
+}
