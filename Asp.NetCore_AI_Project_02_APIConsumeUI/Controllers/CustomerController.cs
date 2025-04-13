1e20c5aa483c25aa597c5e50766e3630ff851840
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 15:34:29 +0300

    Delete & Update Process Added With API Consume

--- Asp.NetCore_AI_Project_02_APIConsumeUI/Controllers/CustomerController.cs
+++ Asp.NetCore_AI_Project_02_APIConsumeUI/Controllers/CustomerController.cs
@@ -51,5 +51,29 @@ namespace Asp.NetCore_AI_Project_02_APIConsumeUI.Controllers
             }
             return View();
         }
+        
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7254/api/Customers?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("CustomerList");
+            }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateCustomer(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage=await client.GetAsync("https://localhost:7254/api/Customers?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCustomerDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
     }
 }
