02a61f3d3e4ea162559d2bf511774337b5dcf5e4
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 14:19:23 +0300

    Migration + Database Process  Added

---
+++ Asp.NetCore_AI_Project_01_APIDemo/Context/APIContext.cs
@@ -0,0 +1,14 @@
+﻿using Asp.NetCore_AI_Project_01_APIDemo.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore_AI_Project_01_APIDemo.Context
+{
+    public class APIContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;initial catalog=AIProjectDB;Integrated Security=true;trustservercertificate=true");
+        }
+        public DbSet<Customer> Customers { get; set; } //Customer bizim Entities Çoğul hali İse Veritabanına yansıyacak isim (Customers)
+    }
+}
