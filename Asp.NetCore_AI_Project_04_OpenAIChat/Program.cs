5ea33fce9a9a5bfe804d9621d6696f536103640c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 22:25:37 +0300

    Open AI Chat Codes 1 Process Added

--- Asp.NetCore_AI_Project_04_OpenAIChat/Program.cs
+++ Asp.NetCore_AI_Project_04_OpenAIChat/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Yapay Zekaya Başlıyoruz!");
-Console.ReadLine();
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        var apiKey = "burayakeygelecek";
+        Console.WriteLine("Lütfen bir soru girin: (örnek 'Merhaba Bugün Hava İzmir'de kaç derece ?')");
+
+        var prompt = Console.ReadLine();
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("Authorization",$"Bearer{apiKey}");
+        var requestBody = new
+        {
+            model = "gpt-3.5-turbo", // Modeli belirtiyoruz
+            messages = new[] // // Mesajları belirtiyoruz
+            {
+                new { role = "system", content = "Sen bir yapay zeka asistanısın." }, // Sistem mesajı
+                new { role = "user", content = prompt } // Kullanıcıdan gelen mesajı ekliyoruz
+            },
+            max_tokens = 100, // Yanıtın maksimum token sayısını belirtiyoruz
+            temperature = 0.7 // Yanıtın rastgeleliğini belirtiyoruz (0-1 arası değer alır)
+        };
+    }
+}
