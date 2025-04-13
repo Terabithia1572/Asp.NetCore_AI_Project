63d6a3abbd442a6bfe3dc3b05a8010bc37d14e75
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-04-13 14:39:58 +0300

    APIConsumeUI Project Created

---
+++ Asp.NetCore_AI_Project_02_APIConsumeUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp.NetCore_AI_Project_02_APIConsumeUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
